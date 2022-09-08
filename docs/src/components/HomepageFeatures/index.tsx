d785273d1085f0afad62b0e1021323054ecf1ba8
Muhammad Rizkillah <65638961+mrizklh@users.noreply.github.com>
2022-09-08 04:51:27 +0000

    "yeah"

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -9,28 +9,23 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Belajar sendiri dengan sukarela (autonomy)',
+    title: 'Boring',
     description: (
       <>
-        Autonomi adalah aspek penting dalam pembelajaran aktif. 
-        riset menunjukkan bahwa siswa yang memiliki kebebasan dalam memilih topik yang akan dipelajari, memiliki kemampuan untuk mengembangkan keterampilan yang lebih baik.
       </>
     ),
   },
   {
-    title: 'Fokus pada relevansi industri (relevance)',
+    title: 'disIntegration',
     description: (
       <>
-        Dunia begitu cepat berubah kurikulum Universitas yang rigid dalam 4 tahun menyediakan keterampilan yang tidak realevan secara praktis bagi industri. untuk itu perlunya kemampuan membaca pasar.
       </>
     ),
   },
   {
-    title: 'Diberdayakan oleh Komnunitas (community)',
+    title: 'on Dream',
     description: (
       <>
-        Mari bergabung dengan komunitas ICP Challenge untuk membangun ekosistem pembelajaran yang lebih baik.
-        dengan adanya komunitas, kita dapat saling berbagi dan saling membantu untuk mencapai tujuan bersama.
       </>
     ),
   },
