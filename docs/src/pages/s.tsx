a77149c4e74e133092956582d472a92bfbdb6e09
Muhammad Rizkillah <65638961+mrizklh@users.noreply.github.com>
2022-10-10 08:10:24 +0000

    "redirect to google search"

---
+++ docs/src/pages/s.tsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const Redirect = () => <div>
+  <meta httpEquiv="refresh" content="0; url=https://www.google.com/search?q=rizkidotme" />
+</div>
+
+module.exports = Redirect;
