6c52aa9c88cf07b8e1711c9250b8a0e544b9ffdc
Muhammad Rizkillah <65638961+mrizklh@users.noreply.github.com>
2022-10-10 08:39:28 +0000

    "fix tsconfig.json"

--- docs/src/pages/s.tsx
+++ docs/src/pages/s.tsx
@@ -1,6 +1,29 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+
+/* rewrite the code:
+root/docs/src/pages/s.tsx
 const React = require('react');
 const Redirect = () => <div>
   <meta httpEquiv="refresh" content="0; url=https://www.google.com/search?q=rizkidotme" />
 </div>
 
 module.exports = Redirect;
+
+to match docusaurus typescript templete */ 
+
+// update s.tsx file: 
+// const React = require('react');
+// const Redirect = () => <div>
+//   <meta httpEquiv="refresh" content="0; url=https://www.google.com/search?q=rizkidotme" />
+// </div>
+
+// module.exports = Redirect;
+
+// to match docusaurus typescript templete
+
+const Redirect = () => <div>
+  <meta httpEquiv="refresh" content="0; url=https://www.google.com/search?q=rizkidotme" />
+</div>
+
+export default Redirect;
